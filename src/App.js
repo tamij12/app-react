8a8d397b4c2231172949b71ae9dc457f0a9ba7fb
tamij12 <tamijangosrz@gmail.com>
2023-10-23 17:59:05 -0600

    initial app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Box from './Box.js';
+import { useState } from 'react';
 
 function App() {
+  // const usersWithGrades = [
+  //   {
+  //     name: 'pepe',
+  //     grade: 10
+  //   },
+  //   {
+  //     name: 'pablo',
+  //     grade: 9
+  //   },
+  //   {
+  //     name: 'ernesto',
+  //     grade: 9.5
+  //   },
+  //   {
+  //     name: 'noe',
+  //     grade: 10
+  //   },
+  //   {
+  //     name: 'ana',
+  //     grade: 8
+  //   }
+  // ]
+  const [user, setUser] = useState('Tania');
+
+  function changeName() {
+    const newName = document.getElementById('nameChangeInput').value;
+    setUser(newName);
+    document.getElementById('nameChangeInput').value= '';
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          // usersWithGrades.map((user, index) => {
+          //   return <Box key={index} user={user.name} grade={user.grade} />
+          // })
+        }
+        Hola, {user}!
+        <input id='nameChangeInput' type='text' placeholder='Change your name'/> 
+        <button onClick={changeName}>Change</button>
       </header>
     </div>
   );
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + './public',
+        publicPath: '/',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    }
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif|ico|json)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    devServer: {
+        port: 3000,
+        historyApiFallback: true,
+    }
+}
